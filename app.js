69e217ed671560ba1fd7713443fd8eaed2119816
ElazzouziNordineBeCode <elazzouzinordine.becode@gmail.com>
2022-06-03 16:00:32 +0200

    Co-authored-by: 4E 65 6F <John4E656F@users.noreply.github.com> Co-authored-by: Benhammou Bilal <BenhamBilal@users.noreply.github.com>

--- app.js
+++
@@ -1,8 +0,0 @@
-// on clic on the image called Cookie.png multiply 2 one on value called cookie-counter-number
-
-var cookieCounter = document.getElementById("cookie-counter-number");
-var cookieButton = document.getElementById("cookie-img");
-
-cookieButton.addEventListener("click", function () {
-  cookieCounter.innerHTML = parseInt(cookieCounter.innerHTML) * 2;
-});

c6ca217424d3c8355d2fe22c838092f2ae891e8c
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-03 16:32:06 +0200

    step 01 - 04

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const cookieClic = document.querySelector(".cookie-clic");
 let counter = 1;
 
 cookieClic.addEventListener("click", () => {
-  score.innerHTML = counter++;
+  score.innerHTML = parseInt(score.innerHTML) + counter;
+});
+
+const labelMultiply = document.querySelector(".label-multiply");
+const cookieMulyiply = document.querySelector(".cookie-multiply");
+let counterMult = parseInt(cookieMulyiply.innerHTML);
+labelMultiply.addEventListener("click", () => {
+  cookieMulyiply.innerHTML = ++counterMult;
+  counter = counterMult
 });

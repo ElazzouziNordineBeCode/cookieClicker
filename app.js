265e3fe415418fabeaeece50da2e750937a43a98
ElazzouziNordineBeCode <elazzouzinordine.becode@gmail.com>
2022-06-03 16:19:46 +0200

    step 01 - 04

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const score = document.querySelector('.cookie-number');
-const cookieClic = document.querySelector('.cookie-clic');
+// increment cookie number after cookie clicking
+
+const score = document.querySelector(".cookie-number");
+const cookieClic = document.querySelector(".cookie-clic");
 let counter = 1;
 
-cookieClic.addEventListener('click', ()=>{
-    score.innerHTML = counter++;
-})
+cookieClic.addEventListener("click", () => {
+  score.innerHTML = counter++;
+});

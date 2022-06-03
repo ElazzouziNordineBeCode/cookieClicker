e29a49060459dc84d4946f91bd7885b7c66e17a4
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-03 16:13:07 +0200

    step 1- 3

---
+++ app.js
@@ -0,0 +1,7 @@
+const score = document.querySelector('.cookie-number');
+const cookieClic = document.querySelector('.cookie-clic');
+let counter = 1;
+
+cookieClic.addEventListener('click', ()=>{
+    score.innerHTML = counter++;
+})
